03d12ddbb4a8b834137ab09489797c0af0d2c381
Paul Lalonde <paul.lalonde@ticksmith.com>
2022-07-07 18:04:01 -0400

    latest Catalina version

--- bin/make-bootable-iso.catalina.sh
+++ bin/make-bootable-iso.catalina.sh
@@ -5,7 +5,7 @@ set -eu
 SELF_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 OS_NAME="Catalina"
-OS_VERSION="10.15.6"
+OS_VERSION="10.15.7"
 INSTALLER_NAME="Install macOS Catalina"
 ISO_NAME="install-macos-10.15-catalina.iso"
 DISK_SIZE=12g
